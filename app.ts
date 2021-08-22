d1770a2c102ee24a6ef9cc53a5b1b7027efe7dcc
Pavol <safarik@lotum.de>
2021-08-22 16:39:08 +0200

    store Fr ersion

--- app.ts
+++ app.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env ts-node
 /* eslint-disable camelcase */
 
-import { command, restPositionals, flag, run, binary, boolean } from 'cmd-ts'
+import { command, restPositionals, flag, run, binary, boolean, option, optional } from 'cmd-ts'
 import { File } from 'cmd-ts/dist/cjs/batteries/fs'
 import fs from 'fs'
 import { cpus } from 'os'
@@ -11,7 +11,7 @@ import { Transform } from 'stream'
 const n_cpus = cpus().length
 console.log(n_cpus)
 
-const randomSleeper = new Transform({
+let randomSleeper = new Transform({
   transform (chunk, encoding, callback) {
     // await setTimeout(() => {}, 5)
     // await new Promise(resolve => setTimeout(resolve, Math.random() * 100))
@@ -23,11 +23,20 @@ const cat = command({
   name: 'mylint',
   args: {
     paths: restPositionals({ type: File }),
-    sleeper: flag({ type: boolean, long: 'sleeper', short: 's' })
+    sleeper: flag({ type: boolean, long: 'sleeper', short: 's' }),
+    new_sleeper: flag({ type: boolean, long: 'new_sleeper', short: 'n' })
   },
-  async handler ({ paths, sleeper }) {
+  async handler ({ paths, sleeper, new_sleeper }) {
     for (const path of paths) {
-      if (sleeper) {
+      if (sleeper || new_sleeper) {
+        if (new_sleeper) {
+          randomSleeper = new Transform({
+            async transform (chunk, encoding, callback) {
+              await new Promise(resolve => setTimeout(resolve, Math.random() * 100))
+              callback(null, chunk)
+            }
+          })
+        }
         fs.createReadStream(path, { highWaterMark: 3 }).pipe(randomSleeper).pipe(process.stdout)
       } else {
         fs.createReadStream(path, { highWaterMark: 3 }).pipe(process.stdout)
